01a883161194417f1d7fb3b98c3005de99e26b77
Karthik Divi <karthikdivi@gmail.com>
2017-11-19 15:26:53 +0530

    changed store api.

--- routes/app.js
+++ routes/app.js
@@ -12,25 +12,41 @@ router.get('/shorten', function(req, res){
   var inputUrl = req.query.url;
   if(inputUrl){
     var id =  utils.getBase64FromBase10(utils.generateRandomNumber(config.maxNum))
-    store.put(id, inputUrl)
-    res.send(config.domain+id)
+    store.put(id, inputUrl, function(error){
+        if(error){
+            res.status(500).send(error);
+        }else{
+            res.send(config.domain+id)
+        }
+    })
   }else{
       res.status(400).send('Please send a valid url in url parameter');
   }
 });
 
 router.get('/all', function(req,res){
-  res.send(store.getAll());
+  store.getAll(function(result, error){
+    if(error){
+      res.status(500).send(error);
+    }else{
+        res.send(result);
+    }
+  });
 });
 
 router.get('/:id', function(req,res){
   var id = req.params.id;
-  var url = store.get(id);
-  if(url){
-      res.redirect(url);
-  }else{
-      res.status(404).send('Not found');
-  }
+  store.get(id, function(result, error){
+    if(error){
+      res.status(500).send(error);
+    }else{
+        if(result){
+          res.redirect(result);
+        }else{
+            res.status(404).send('Not found');
+        }
+    }
+  });
 });
 
 module.exports = router;
